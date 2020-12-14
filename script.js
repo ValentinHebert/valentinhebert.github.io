f16888ca415399de63cb60dc75b60dee172a7b70
Yolanare <y09a3115@gmail.com>
2020-12-14 16:54:45 +0100

    video: fixes

--- script.js
+++ script.js
@@ -395,7 +395,7 @@ function init() {
             if(vidTile == document.querySelector('.video-list').firstChild) { vidSelect(0); };
             vidTile.addEventListener('click', function() {
                 if(this.classList.contains('focus') != true) {
-                    vidSelect(600);
+                    vidSelect(750);
                     setTimeout(function() { swup.scrollTo(document.body, 0); }, 900);
                 }
             });
